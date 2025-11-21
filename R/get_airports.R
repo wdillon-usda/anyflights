f3c603e3e78cb1ffe9efda99f575f593f2d3aee5
Whalen Dillon <208065492+wdillon-usda@users.noreply.github.com>
2025-11-21 11:28:37 -0500

    Filter airports for US and Puerto Rico

--- R/get_airports.R
+++ R/get_airports.R
@@ -64,7 +64,7 @@ get_airports <- function(dir = NULL) {
     
   # tidy up the data a bit
   airports <- airports_raw %>%
-    dplyr::filter(country == "United States", faa != "") %>%
+    dplyr::filter(country %in% c("United States", "Puerto Rico"), faa != "") %>%
     dplyr::select(faa, name, lat, lon, alt, tz, dst, tzone) %>%
     dplyr::group_by(faa) %>% 
     dplyr::slice(1) %>% 
